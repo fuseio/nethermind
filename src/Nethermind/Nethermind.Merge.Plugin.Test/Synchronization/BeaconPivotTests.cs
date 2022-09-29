a6f60696f0ad4e78ac98819033467760173f43a9
Amirul Ashraf <asdacap@gmail.com>
2022-09-29 19:35:35 +0800

    Fix test

--- src/Nethermind/Nethermind.Merge.Plugin.Test/Synchronization/BeaconPivotTests.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/Synchronization/BeaconPivotTests.cs
@@ -15,6 +15,7 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System.Collections.Generic;
 using FluentAssertions;
 using Nethermind.Blockchain;
 using Nethermind.Blockchain.Synchronization;
